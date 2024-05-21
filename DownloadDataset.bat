1ad59682cfe2e494fb3ec0c99e2391ee68e2159c
yutadd <spyk4m49@gmail.com>
2024-05-21 18:23:11 +0900

    [add]opencv sample [add]download batch file

---
+++ DownloadDataset.bat
@@ -0,0 +1,3 @@
+curl https://vignette.wikia.nocookie.net/rap/images/5/54/IMG_20201004_231545.jpg/revision/latest?cb=20201004174755 -o ./.datasets/sample.webp
+curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml -o ./.datasets/haarcascade_frontalface_default.xml
+curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml -o ./.datasets/haarcascade_eye.xml
