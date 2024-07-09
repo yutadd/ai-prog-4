560f39352e67b070e30f3b281d2c1085e5a57813
yutadd <spyk4m49@gmail.com>
2024-07-09 16:46:32 +0900

    言語学１００本ノック

--- DownloadDataset.bat
+++ DownloadDataset.bat
@@ -1,3 +1,4 @@
 curl https://vignette.wikia.nocookie.net/rap/images/5/54/IMG_20201004_231545.jpg/revision/latest?cb=20201004174755 -o ./.datasets/sample.webp
 curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml -o ./.datasets/haarcascade_frontalface_default.xml
 curl https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml -o ./.datasets/haarcascade_eye.xml
+curl https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv -o ./.datasets/all_country.csv
